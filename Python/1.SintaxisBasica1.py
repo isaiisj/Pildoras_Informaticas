fb9a6286889298337640d9d98479ea304a4a0ea9
Isai <47548422+isaiisj@users.noreply.github.com>
2023-12-07 13:52:20 -0600

    Create 1.SintaxisBasica1.py

---
+++ Python/1.SintaxisBasica1.py
@@ -0,0 +1,10 @@
+print("Hola alumnos)
+print("Hola alumnos); print("Adios Alumnos")
+#Este es un comentario
+mi_nombre = "Mi nombre es Isai"
+mi_nombre = "Mi nombre es\
+isai"
+a = 0
+for i in range(5):
+  a += 1
+  print(a)
