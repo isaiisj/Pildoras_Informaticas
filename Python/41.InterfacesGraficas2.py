af9c76824efb2065aa9d7c2f0a506952091bac9c
Isai <47548422+isaiisj@users.noreply.github.com>
2024-03-14 03:53:42 -0600

    Create 41.InterfacesGraficas2.py

---
+++ Python/41.InterfacesGraficas2.py
@@ -0,0 +1,18 @@
+form tkinter import *
+raiz = Tk()
+raiz.title("Ventanade pruebas")
+#raiz.resizable(True,False)
+raiz.iconbitmap("gato.ico")
+#raiz.geometry("650x350")
+raiz.config(bg="blue")
+miFrame = Frame()
+miFrame.pack(fill="both", expand="True")
+miFrame.config(bg="red") #la raiz se adapta al frame
+miFrame.config(width="650", height="350)
+miFrame=config(bd=35)
+miFrame.config(relief="groove")
+miFrame.config(cursor="pirate")
+raiz.mainloop()
+
+'''Todo lo que se aplica al frame
+ se puede aplicar a la raiz '''
