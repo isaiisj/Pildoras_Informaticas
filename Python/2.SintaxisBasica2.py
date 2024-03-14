9ea30aa1d544d488a0b1739b83281be67fee3bb2
Isai <47548422+isaiisj@users.noreply.github.com>
2024-03-14 03:27:01 -0600

    Create 2.SintaxisBasica2.py

---
+++ Python/2.SintaxisBasica2.py
@@ -0,0 +1,15 @@
+nombre = 5
+type(nombre)
+nombre = "juan"
+type(nombre)
+nombre = 5.2
+type(nombre)
+mensaje = """ Esto es
+    un mensaje
+    con tres saltos"""
+numero1 = 5
+numero2 = 7
+if numero1 > numero2:
+  print("El numero 1 es mayor")
+else:
+  print("El numero 2 es mayor")
