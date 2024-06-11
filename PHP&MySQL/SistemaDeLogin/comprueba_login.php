97def378a8a7b7498c23195d8f26d03009265f90
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-10 23:18:25 -0600

    Create comprueba_login.php

---
+++ PHP&MySQL/SistemaDeLogin/comprueba_login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Compruba login</title>
+  </head>
+  <body>
+    <?php
+    try{
+      $base=new PDO("mysql:host=localhost; dbname=prueba", "root", "");
+      
+      $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      
+      $sql="SELECT * FROM USUARIOS_PASS WHERE USUARIOS= :login AND PASSWORD= :password";
+      
+      $resultados=$base->prepare(sql);
+      
+      $login=htmlentities(addslashes($_POST["login"]));
+      
+      $password=htmlentities(addslashes($_POST["password"]));
+      
+      $resultado->bindValue(":login",$login);
+      
+      $resultado->bindValue(":password",$password);
+      
+      $resultado->execute();
+      
+      $numero_registro=$resultado->rowCount();
+
+      if($numero_registro!=0){
+        echo "<h2> Adelante </h2>";
+      }else{
+        header("location.login.php");
+      }
+        
+    }catch(Exception $e){
+      die("Error: ". $e ->getMessage());
+    }
+    ?>
+  </body>
+</html>
