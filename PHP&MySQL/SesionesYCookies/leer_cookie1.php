9e1055d871627d225cf30edee466946dad97b5c0
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-11 02:18:26 -0600

    Create leer_cookie1.php

---
+++ PHP&MySQL/SesionesYCookies/leer_cookie1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Leer cookie 1</title>
+  </head>
+  <body>
+    <?php 
+      echo 4_COOKIE["prueba"];
+    ?>
+  </body>
+</html>
