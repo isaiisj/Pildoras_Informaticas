2f94886b2b0c24601d9cbbf7c8713b045271136b
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-10 23:25:29 -0600

    Create login.php

---
+++ PHP&MySQL/SistemaDeLogin/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+  <body>
+    <h1>Introduce tus datos</h1>
+    <form action="comprueba_login.php" method="post">
+      <label>Login:</label>
+      <input type="text" name="login">
+      <label>Password</label>
+      <input type="password" name="password">
+      <input type="subimt" name="enviar" value="LOGIN">
+    </form>
+  </body>
+</html>
