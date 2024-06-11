fa0821888fca241d5b834a433d060daa20a2b8e0
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-10 23:34:21 -0600

    Create usuarios_registrados1.php

---
+++ PHP&MySQL/SistemaDeLogin/usuarios_registrados1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Pagina para usuarios registrados</title>
+  </head>
+  <body>
+    <h1>Bienvenidos Usuarios</h1>
+
+    <p>Esto es info para usuarios registrados</p>
+  </body>
+</html>
