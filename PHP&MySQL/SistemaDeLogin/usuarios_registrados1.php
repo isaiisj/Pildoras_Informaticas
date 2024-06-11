cd0a2d45f6fb7654c39dcbcf72ca8c1ed5f9c2ab
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-11 02:00:10 -0600

    Update usuarios_registrados1.php

--- PHP&MySQL/SistemaDeLogin/usuarios_registrados1.php
+++ PHP&MySQL/SistemaDeLogin/usuarios_registrados1.php
@@ -20,5 +20,17 @@
     <?php 
         echo "Hola".$_SESSION["usuario"]."<br>";
     ?>
+
+    <p><a href="cierre.php">Cierre Sesión</a></p>
+  <table>
+  	<tr>
+  		<th colspan="3">Zona usuarios Registrados</th>
+  	</tr>
+  	<tr>
+  		<td><a href="usuarios_registrados2.php">Página 1</a></td>
+  		<td><a href="usuarios_registrados3.php">Página 2</a></td>
+  		<td><a href="usuarios_registrados4.php">Página 3</a></td>
+  	</tr>
+  </table>
   </body>
 </html>
