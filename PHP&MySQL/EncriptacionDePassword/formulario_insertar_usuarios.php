a7ae7ca609f8d80cb74e1e13c26d3240e334baaa
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-11 23:38:56 -0600

    Create formulario_insertar_usuarios.php

---
+++ PHP&MySQL/EncriptacionDePassword/formulario_insertar_usuarios.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+    
+    <head>
+    
+        <meta charset="utf-8">
+        <title>Documento sin título</title>
+        
+        
+    </head>
+    
+    <body>
+    <h1>REGÍSTRATE</h1>
+    
+        <form action="pagina_insertar_usuarios.php" method="post">
+            <label>Usuario</labeñ>
+            <input type="text" name="usu" id="usu"><br>
+             <label> Contraseña </label>
+             <input type="text" name="contra" id="contra"><br>
+            <input type="submit" name="enviando" value="¡Dale!">
+        </form>
+    
+    </body>
+    
+</html>
