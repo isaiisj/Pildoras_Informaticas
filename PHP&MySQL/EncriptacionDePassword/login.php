71b529c34b8d61fbb8b6b898d08b37dc0badbf81
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-12 00:01:38 -0600

    Create login.php

---
+++ PHP&MySQL/EncriptacionDePassword/login.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+
+</head>
+
+<body>
+
+<h1> INTRODUCE TUS DATOS</h1>
+
+<form action="comprueba_login.php" method="post">
+
+<label>Login: </label>
+<input type="text" name="login"><br>
+<label>Password:</label>
+<input type="password" name="password"><br>
+<input type="submit" name="enviar" value="LOGIN">
+
+</form>
+
+</body>
+</html>
