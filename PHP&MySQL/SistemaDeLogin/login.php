810add79100b3efda1bccc54e1eb49825533c8d7
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-12 00:02:10 -0600

    Update login.php

--- PHP&MySQL/SistemaDeLogin/login.php
+++ PHP&MySQL/SistemaDeLogin/login.php
@@ -8,9 +8,9 @@
     <h1>Introduce tus datos</h1>
     <form action="comprueba_login.php" method="post">
       <label>Login:</label>
-      <input type="text" name="login">
+      <input type="text" name="login"><br>
       <label>Password</label>
-      <input type="password" name="password">
+      <input type="password" name="password"><br>
       <input type="subimt" name="enviar" value="LOGIN">
     </form>
   </body>
