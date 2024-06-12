299f17d3d89a923d819188987c8a625a20fcdd9e
Isai <47548422+isaiisj@users.noreply.github.com>
2024-06-11 23:57:06 -0600

    Update formulario_insertar_usuarios.php

--- PHP&MySQL/EncriptacionDePassword/formulario_insertar_usuarios.php
+++ PHP&MySQL/EncriptacionDePassword/formulario_insertar_usuarios.php
@@ -13,7 +13,7 @@
     <h1>REGÍSTRATE</h1>
     
         <form action="pagina_insertar_usuarios.php" method="post">
-            <label>Usuario</labeñ>
+            <label>Usuario</label>
             <input type="text" name="usu" id="usu"><br>
              <label> Contraseña </label>
              <input type="text" name="contra" id="contra"><br>
