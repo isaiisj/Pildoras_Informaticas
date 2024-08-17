6d3734dfb4db466452ed3b3f42ac032e71a04aa5
Isai <47548422+isaiisj@users.noreply.github.com>
2024-08-17 01:27:39 -0600

    Create index.php

---
+++ PHP&MySQL/SubirImagenesAlServidor/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+</head>
+<body>
+
+<form action="datosImagen.php" method="post" enctype="multipart/form-data">
+
+		<label for="imagen">Imagen:</label></td>
+
+		<input type="file" name="imagen" size="20">
+	
+		<input type="submit" value="Enviar Imagen">
+	
+</form>	
+</body>
+</html>
