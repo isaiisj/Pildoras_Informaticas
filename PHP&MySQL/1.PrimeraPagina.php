6eb785ca3b874527809519d7c39561bd77dadabb
Isai <47548422+isaiisj@users.noreply.github.com>
2024-08-17 15:58:19 -0600

    Create 1.PrimeraPagina.php

---
+++ PHP&MySQL/1.PrimeraPagina.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	
+</head>
+<body>
+
+<?php
+	
+	print "Bienvenidos al curso de PHP <br>";
+  print "Hola alumnos <br>";
+  print "Hasta la proxima";
+?>
+
+
+</body>
+</html>
