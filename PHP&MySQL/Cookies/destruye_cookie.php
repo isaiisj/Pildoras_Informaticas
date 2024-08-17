add4a49e618078c35bd3bacd1ebcc708524bc7d1
Isai <47548422+isaiisj@users.noreply.github.com>
2024-08-17 16:13:38 -0600

    Create destruye_cookie.php

---
+++ PHP&MySQL/Cookies/destruye_cookie.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+  <?php
+
+    setcookie("prueba", "Esta es la información de nuestra primera cookie", time()-1, "localhost/zona_contenidos/");
+  ?>
+	
+</body>
+</html>
